8e0a3e8877a8089d8d88f788f41bf9e46b5a8d1d
Azimkhan93 <aabdulsatarov@gmail.com>
2022-02-05 01:34:43 +0500

    feat: herbalife project

---
+++ src/js/scripts.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('#preloader').fadeOut(500);
+});
+
+
+
+// $(function() {
+// 	$('#language').on('click', function(event) {
+// 		chooselanguage();
+// 	})
+
+// 	function chooselanguage() {
+// 		$('#lang-choice').show(500);
+		
+// 	};
+// })
