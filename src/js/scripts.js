fb8a7185d877583cd57aae378187e3ccc1bb7380
Azimkhan93 <aabdulsatarov@gmail.com>
2022-02-06 01:42:12 +0500

    fix: js added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,21 @@
-$(function(){
+$(function() {
 	$('#preloader').fadeOut(500);
-});
+
+	$('.button-lang').on('click', function(event) {
+		chooselanguage();
+	})
+
+	$('.button-date').on('click', function(event){
+		$('.button-date').removeClass('active')
+		$(this).addClass('active')
+		$('.table-schedule').removeClass('active')
+		const tableId = $(this).data("table")
+		$(`#${tableId}`).addClass('active')
+	})
+	
+	function chooselanguage() {
+		$('#lang-choice').fadeOut(500);
+	};
+})
 
 
-
-// $(function() {
-// 	$('#language').on('click', function(event) {
-// 		chooselanguage();
-// 	})
-
-// 	function chooselanguage() {
-// 		$('#lang-choice').show(500);
-		
-// 	};
-// })
